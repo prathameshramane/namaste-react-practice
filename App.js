1674878505375ed0d4d574adc64c1d0abcb59230
Prathamesh Ramane <ppramane@gmail.com>
2024-02-27 14:54:18 +0530

    Lecture 2 Parcel Bundler

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const heading = React.createElement("h1", {}, "Namaste from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {}, "Namaste from Parcel!");
 const heading2 = React.createElement("h2", {}, "Heading 2");
 const container = React.createElement("div", { id: "container" }, [
   heading,
